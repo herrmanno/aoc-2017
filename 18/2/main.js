b8ad491f14360fa955af5f70dde6c9b27eb80a05
Oliver Herrmann <herrmann@livebook.de>
2017-12-19 10:30:28 +0100

    Day 18

--- 18/2/main.js
+++ 18/2/main.js
@@ -4,12 +4,14 @@ const instructions = fs.readFileSync(__dirname + "/" + "input.txt").toString().s
 
 class Program {
 
-    constructor(instructions, id, queue) {
+    constructor(instructions, id, sndQueue, rcvQueue) {
         this.instructions = instructions
         this.id = id
-        this.queue = queue
+        this.sndQueue = sndQueue
+        this.rcvQueue = rcvQueue
+        this.send = 0
         this.ip = 0
-        this.registers = new Proxy({}, {
+        this.registers = new Proxy({p: id}, {
             get(target, key) {
                 return target[key] || 0
             },
@@ -32,54 +34,57 @@ class Program {
         return this.registers[s] = this.get(v)
     }
 
-    receive(regName) {
-        while (this.queue.length === 0) {
-            ;
-        }
-        this.receiving = false
-        this.set(regName, this.queue.shift())
-    }
+}
 
-    run() {
-        let sendCount = 0
-        while (this.ip < this.instructions.length) {
-            const [cmd, arg1, arg2] = this.instructions[this.ip].split(" ")
-            switch (cmd) {
-                case "snd": this.queue.push(this.get(arg1)); sendCount++ ; break
-                case "set": this.set(arg1, arg2); break
-                case "add": this.set(arg1, this.get(arg1) + this.get(arg2)); break
-                case "mul": this.set(arg1, this.get(arg1) * this.get(arg2)); break
-                case "mod": this.set(arg1, this.get(arg1) % this.get(arg2)); break
-                case "rcv": this.receive(arg1); break
-                case "jgz": {
-                    if (this.get(arg1) > 0) {
-                        this.ip += (this.get(arg2) - 1)
-                    }
+function run(prog) {
+    let ic = prog.ip < prog.instructions.length ? 0 : -1    
+    loop: while (prog.ip < prog.instructions.length) {
+        const [cmd, arg1, arg2] = prog.instructions[prog.ip].split(" ")
+        switch (cmd) {
+            case "snd": prog.sndQueue.push(prog.get(arg1)); prog.send++ ; break
+            case "set": prog.set(arg1, arg2); break
+            case "add": prog.set(arg1, prog.get(arg1) + prog.get(arg2)); break
+            case "mul": prog.set(arg1, prog.get(arg1) * prog.get(arg2)); break
+            case "mod": prog.set(arg1, prog.get(arg1) % prog.get(arg2)); break
+            case "rcv": {
+                if (prog.rcvQueue.length) {
+                    prog.set(arg1, prog.rcvQueue.shift())
+                    break
+                } else {
+                    break loop
                 }
             }
-            this.ip++
+            case "jgz": {
+                if (prog.get(arg1) > 0) {
+                    prog.ip += (prog.get(arg2) - 1)
+                }
+            }
         }
-        return sendCount
+        prog.ip++
+        ic++
     }
+    return ic
 }
 
 const q0_1 = []
 const q1_0 = []
-const prog0 = new Program(instructions, 0, q0_1)
-const prog1 = new Program(instructions, 0, q1_0)
+const prog0 = new Program(instructions, 0, q0_1, q1_0)
+const prog1 = new Program(instructions, 1, q1_0, q0_1)
 
 
-const sndCountProm = new Promise((r) => r(prog0.run()))
-new Promise((r) => r(prog1.run()))
-
-
-sndCountProm.then(sndCount => {
-    console.log(sndCount)
-})
-
-
-setInterval(() => {
-    if (prog0.receiving && prog1.receiving) {
-        console.log("Deadlock")
+let i = 0
+const progs = [prog0, prog1]
+let state = [-1, -1]
+while (true) {
+    state[i] = run(progs[i])
+    if (state.every(ic => ic === -1)) {
+        console.log("Both programs finished")
+        break
+    } else if(state.every(ic => ic === 0)) {
+        console.log("Deadlock:")
+        break
     }
-}, 1000)
+    i ^= 1
+}
+
+console.log(`Program 1 send ${prog1.send} times`)
